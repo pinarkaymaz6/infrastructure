e765c8f45ba241557c566aa62777156ce588d9cf
Pinar Kaymaz <pinarkaymaz6@gmail.com>
2021-12-26 01:56:44 +0100

    Add test case

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print("App started.")
+import requests
+
+
+def get_cat_fact():
+    response = requests.get("https://catfact.ninja/fact")
+    if response.status_code == 200:
+        return response.json().get("fact")
+    return "No fact found today :("
+
+
+def main():
+    print("Cat Fact: ", get_cat_fact())
+
+
+if __name__ == "__main__":
+    main()
