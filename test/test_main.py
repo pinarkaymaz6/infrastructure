32309c9d65e8ab5243446a352ecfcc574d5635b2
Pinar Kaymaz <pinarkaymaz6@gmail.com>
2021-12-26 02:29:49 +0100

    Reorder jobs

--- test/test_main.py
+++ test/test_main.py
@@ -1,16 +1,5 @@
 from src.main import get_cat_fact
 from unittest.mock import Mock, patch
-
-
-# @patch("requests.get")
-# def test_get_cat_fact(mock_req):
-#     response_m = Mock()
-#     response_m.status_code = 200
-#     response_m.json = Mock(return_value={"fact": "some-cat-fact", "length": "42"})
-#     mock_req.return_value = response_m
-#     assert get_cat_fact() == "some-cat-fact"
-
-
 import unittest
 
 
